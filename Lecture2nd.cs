d3a55c45c8bbb2c1772e0965ed4df1ad9590a097
ram-459 <Prataapsingh98@gmail.com>
2025-07-09 10:54:23 +0530

    new learning

---
+++ Lecture2nd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAM_23SOECE11606
+{
+    internal class Lecture2nd
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            int result = a++ + b++ + ++b;
+            //5 +5 + 7 
+            Console.WriteLine(result);
+        }
+    }
+}
